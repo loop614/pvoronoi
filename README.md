7f06ceb5d9ce8ba8ac48a96903c75415d1ed4902
loop <tony.kusic@gmail.com>
2023-01-30 22:47:51 -0500

    added pre-commit, fixed adding seeds to image

---
+++ README.md
@@ -0,0 +1,4 @@
+[Voronoi diagram](https://en.wikipedia.org/wiki/Voronoi_diagram) png generator
+
+### next TODO
+1 . optimize color fillings
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+from __future__ import annotations
+
 import math
 import random
-from typing import NamedTuple, List, Tuple
-from PIL import Image
+from bisect import bisect_left
+from typing import NamedTuple
+
 import numpy as np
+from PIL import Image
 
 factor = 100
 height = factor * 9
 width = factor * 16
 number_of_seeds = 16
+seed_size = 10
 
 
 class Point(NamedTuple):
@@ -24,8 +29,20 @@ class Color(NamedTuple):
     b: int = 0
 
 
-def generate_seeds() -> List[Point]:
-    # TODO: consider going full random
+def generate_seeds() -> list[Point]:
+    seeds = []
+    while True:
+        pos_x = random.randint(0, width)
+        pos_y = random.randint(0, height)
+        seeds.append(Point(x=pos_x, y=pos_y))
+
+        if len(seeds) == number_of_seeds:
+            break
+
+    return seeds
+
+
+def generate_seeds_random_by_square() -> list[Point]:
     seeds = []
     division_number = math.sqrt(number_of_seeds)
     width_box = math.ceil(width / division_number)
@@ -33,8 +50,15 @@ def generate_seeds() -> List[Point]:
 
     for i in range(0, width - 1, width_box):
         for j in range(0, height - 1, height_box):
-            max_y = j + height_box - 1 if j + height_box - 1 < height else height - 1
-            max_x = i + width_box - 1 if i + width_box - 1 < width else width - 1
+            if j + height_box - 1 < height:
+                max_y = j + height_box - 1
+            else:
+                max_y = height - 1
+
+            if i + width_box - 1 < width:
+                max_x = i + width_box - 1
+            else:
+                max_x = width - 1
 
             pos_x = random.randint(i, max_x)
             pos_y = random.randint(j, max_y)
@@ -47,31 +71,56 @@ def generate_seeds() -> List[Point]:
 
 
 def add_seeds_to_image(image, seeds):
+    seed_size_range = range(
+        -math.floor(seed_size / 2),
+        math.floor(seed_size / 2),
+    )
     for seed in seeds:
-        if image[seed.y][seed.x] is not None:
-            image[seed.y][seed.x] = 0xFFFFFF
+        for size_x in seed_size_range:
+            for size_y in seed_size_range:
+                x = seed.x + size_x
+                y = seed.y + size_y
+                if image[y][x] is not None:
+                    image[y][x] = 0x000000
 
     return image
 
 
-def get_next_color_value(color_value: int, start: int = 255, tick=True) -> int:
-    # TODO: finish the distinct color generator, like binary search just go both ways
-    if color_value == 0:
-        return 255
+def get_next_color_value(color_value: int, diversity_colors: list[int]) -> int:
+    current_index = diversity_colors.index(color_value)
+    if current_index + 1 < len(diversity_colors):
+        return diversity_colors[current_index + 1]
 
-    if color_value == 255:
-        return 127
-
-    if color_value == 127:
-        return 169
-
-    if color_value == 169:
-        return 64
-
-    return 0
+    return diversity_colors[0]
 
 
-def generate_rgb_diversity_arrays(n: int) -> Tuple[List[int], List[int], List[int]]:
+def diversity_numbers_for_colors() -> list[int]:
+    pivot: list[int] = [0, 255]
+    pair: list[int] = []
+    to_be_added: list[int] = []
+    res: list[int] = [0, 255]
+    while len(pivot) < 255:
+        for one in pivot:
+            if len(pair) != 2:
+                pair.append(one)
+            if len(pair) == 2:
+                next_one = (pair[0] + pair[1]) // 2
+                to_be_added.append(next_one)
+                pair.pop(0)
+
+        pair = []
+        for next_one in to_be_added:
+            index = bisect_left(pivot, next_one)
+            pivot.insert(index, next_one)
+            res.append(next_one)
+            to_be_added = []
+
+    return res
+
+
+def generate_rgb_diversity_arrays(
+    n: int,
+) -> tuple[list[int], list[int], list[int]]:
     red = []
     green = []
     blue = []
@@ -81,6 +130,7 @@ def generate_rgb_diversity_arrays(n: int) -> Tuple[List[int], List[int], List[in
     red_value = 0
     green_value = 0
     blue_value = 0
+    diversity_numbers = diversity_numbers_for_colors()
 
     for i in range(n):
         red.append(red_value)
@@ -88,19 +138,19 @@ def generate_rgb_diversity_arrays(n: int) -> Tuple[List[int], List[int], List[in
         blue.append(blue_value)
 
         if go_red:
-            red_value = get_next_color_value(red_value)
+            red_value = get_next_color_value(red_value, diversity_numbers)
             go_red, go_green, go_blue = False, True, False
         elif go_green:
-            green_value = get_next_color_value(green_value)
+            green_value = get_next_color_value(green_value, diversity_numbers)
             go_red, go_green, go_blue = False, False, True
         elif go_blue:
-            blue_value = get_next_color_value(blue_value)
+            blue_value = get_next_color_value(blue_value, diversity_numbers)
             go_red, go_green, go_blue = True, False, False
 
     return red, green, blue
 
 
-def generate_colors(seeds: List[Point]) -> List[Color]:
+def generate_colors(seeds: list[Point]) -> list[Color]:
     colors = []
     r, g, b = generate_rgb_diversity_arrays(len(seeds))
     for i in range(len(seeds)):
@@ -121,26 +171,34 @@ def get_closest_seed(needle, seeds):
     return i
 
 
-def add_voronoi_diagrams(image, seeds, colors: List[Color]):
+def add_voronoi_diagrams(image, seeds, colors: list[Color]):
     # TODO: circle about the seeds one by one, until you hit color
     for x in range(width):
         for y in range(height):
             closest_seed_index = get_closest_seed(Point(x=x, y=y), seeds)
-            image[y][x] = (colors[closest_seed_index].r, colors[closest_seed_index].g, colors[closest_seed_index].b)
+            image[y][x] = (
+                colors[closest_seed_index].r,
+                colors[closest_seed_index].g,
+                colors[closest_seed_index].b,
+            )
 
     return image
 
 
 def main():
-    assert math.sqrt(number_of_seeds) % 10 != 0, 'seeds needs to be a factor of 4 for now, its more pretty that way'
-    assert height * width / 4 > number_of_seeds, 'this many seeds wont fit to this small image'
+    assert math.sqrt(
+        number_of_seeds,
+    ) % 10 != 0, 'seeds needs to be a factor of 4 for now'
+    assert height * width / 4 > number_of_seeds, \
+        'this many seeds wont fit to this small image'
     seeds = generate_seeds()
+    # seeds = generate_seeds_random_by_square()
     colors = generate_colors(seeds)
     image = np.zeros((height, width, 3), np.uint8)
-    image = add_seeds_to_image(image, seeds)
     image = add_voronoi_diagrams(image, seeds, colors)
+    image = add_seeds_to_image(image, seeds)
     im = Image.fromarray(image).convert('RGB')
-    im.save("output.png")
+    im.save('output.png')
 
     return 0
 
